db03bc3a6e6734f33f0f4a3177f01f2f73e3cb52
Damien Katz <me@damienkatz.com>
2025-05-28 06:20:53 -0700

    oops
    use file path undecorated in generate_example_run_markdown.sh

--- generate_example_run_markdown.sh
+++ generate_example_run_markdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "" > example_run.md
 for file in $(ls example_run/*.png | sort); do
-  echo "![$file](example_run/$file)" >> example_run.md
+  echo "![$file]($file)" >> example_run.md
 done
